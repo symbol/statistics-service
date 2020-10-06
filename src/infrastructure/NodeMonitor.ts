92d7768c5d6f2ede21467a2da99d99c62ed8ffa4
unknown <olegomm@gmail.com>
2020-10-06 16:24:05 +0300

    add db to NodeMonitor. Add scheduler

--- src/infrastructure/NodeMonitor.ts
+++ src/infrastructure/NodeMonitor.ts
@@ -1,20 +1,51 @@
+import * as config from '@src/config';
+import NodeModel, { INode } from '@src/DataBase/models/Node';
+
 class NodeMonitor {
-    private visitedNodes: string[];
-    private nodeList: string[];
+    private visitedNodes: INode[];
+    private nodeList: INode[];
     private currentNodeIndex: number;
+    private isRunning: boolean;
 
     constructor() {
         this.visitedNodes = [];
         this.nodeList = [];
         this.currentNodeIndex = 0;
+        this.isRunning = false;
     }
 
-    public start = () => {
+    public start = async () => {
+        this.isRunning = true;
         this.clear();
+
+        await this.main();
+
+        if(this.isRunning) {
+            await this.updateCollection();
+            setTimeout(
+                () => this.start(), 
+                config.monitor.NODE_MONITOR_SCHEDULE_INTERVAL
+            );
+        }
     }
 
     public stop = () => {
+        this.isRunning = false;
         this.clear();
+    }
+
+    private main = async (): Promise<any> => {
+        let isFinished = false;
+
+        if(!this.isRunning)
+            return;
+
+        if(isFinished)
+            return Promise.resolve();
+    }
+
+    private fetchNodeList = async (nodeUrl: string): Promise<Array<INode>> => {
+        return [];
     }
 
     private clear = () => {
@@ -23,25 +54,35 @@ class NodeMonitor {
         this.currentNodeIndex = 0;
     }
 
-    private updateCollection = () => {
-
+    private updateCollection = async (): Promise<any> => {
+        await NodeModel.remove({}).exec();
+        await NodeModel.insertMany(this.nodeList);
     }
 
     private checkAPINode = (nodeUrl: string): Promise<boolean> => {
         return Promise.resolve(true);
     }
 
-    private addNodeToList = (nodeUrl: string) => {
+    private addNodeToList = (node: INode) => {
+        //TODO: replace with MAP
+        if(!!this.nodeList.find(addedNode => 
+            addedNode.publicKey === node.publicKey
+        ))
+            return;
+        this.nodeList.push(node);
+    }
+
+    private removeNodeFromList = (node: INode) => {
 
     }
 
-    private removeNodeFromList = (nodeUrl: string) => {
-
+    private addNodeToVisited = (node: INode) => {
+        this.visitedNodes.push(node);
     }
 
-    private addNodeToVisited = (nodeUrl: string) => {
-
+    private isNodeVisited = (node: INode): boolean => {
+        return !!this.visitedNodes.find(visitedNode => 
+            visitedNode.publicKey === node.publicKey
+        );
     }
-
-
 }
