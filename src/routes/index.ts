e79705f633b55c5e82f85f7c32f20981adb78af7
unknown <olegomm@gmail.com>
2020-10-06 16:57:31 +0300

    update /nodes route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Express, Request, Response } from 'express';
+import NodeModel from '@src/DataBase/models/Node';
 
 export class Routes {
     static register = async (app: Express) => {
-        app.get('/nodes', (req: Request, res: Response) => {
-            res.send('/nodes');
+        app.get('/nodes', async (req: Request, res: Response) => {
+            const nodes = await NodeModel.find().exec();
+            res.send(nodes);
         });
     }
 }
