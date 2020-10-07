ad652615171fb7cdd34da3c56329a63083910c6a
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-10-07 22:15:37 +0800

    added isAPIRole function

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,9 @@ export const stringToArray = (str: string | undefined): Array<any> => {
     catch(e) {};
     return result;
 }
+
+export const isAPIRole = (roleType: number): boolean => {
+    // https://github.com/nemtech/symbol-openapi/blob/2c3daf4ca50e5da43c6ab36706825c9599102b36/spec/core/node/schemas/RolesTypeEnum.yml#L10
+    const RolesTypeEnum = [2,3,6,7];
+    return !!RolesTypeEnum.find(role => role === roleType);
+}
