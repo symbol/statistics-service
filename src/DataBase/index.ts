f10de605849e862e186bbfd012cd31009bd918a0
unknown <olegomm@gmail.com>
2020-10-08 15:47:01 +0300

    add access methods to db module

--- src/DataBase/index.ts
+++ src/DataBase/index.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as config from '@src/config';
-export * as models from './models';
+import * as models from './models';
 
 export class DataBase {
 	static connect = async () => {
@@ -12,4 +12,21 @@ export class DataBase {
 		}
 		console.log('[DataBase]: Connected to MongoDB');
 	};
+
+	static getNodeList = (): Promise<models.Node.NodeDocument[]> => {
+		return models.Node.default.find().exec();
+	}
+
+	static getNodeByPublicKey = (publicKey: string): Promise<models.Node.NodeDocument | null> => {
+		return models.Node.default.findOne({publicKey}).exec();
+	}
+
+	static updateNodeList = async (nodeList: models.Node.INode[]): Promise<void> => {
+		await models.Node.default.remove({}).exec();
+		await models.Node.default.insertMany(nodeList);
+	}
+
+	static updateNode = async (node: models.Node.INode): Promise<void> => {
+		await models.Node.default.findOneAndUpdate({publicKey: node.publicKey}, node).exec();
+	}
 }
