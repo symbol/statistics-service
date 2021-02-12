71a5106ac6bf1f9be3a44693ce8b5a099c034048
Oleg Makarenko <olegomm@gmail.com>
2021-02-12 16:28:54 +0200

    add openapi nr generation script

--- scripts/post-install.sh
+++
@@ -1,9 +0,0 @@
-echo "Running post install hook..."
-cd node_modules
-cd symbol-node-rewards
-cd packages
-cd api-spec
-npm install
-npm run build
-echo "symbol-node-rewards api-spec successfully built"
-exit 0
---
+++ src/scripts/build-clients.sh
@@ -0,0 +1,6 @@
+
+echo "Running build of Node Rewards client..."
+rimraf src/models/NodeRewards/build src/models/NodeRewards/gen-src &&
+swagger-cli validate src/models/NodeRewards/openapi3.yml
+openapi-generator-cli generate -i src/models/NodeRewards/openapi3.yml -g typescript-fetch -o gen-src --additional-properties=typescriptThreePlus=true,withInterfaces=true,useSingleRequestParameter=false,supportsES6=true
+exit 0
