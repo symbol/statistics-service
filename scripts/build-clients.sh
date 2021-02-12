5ecc7d87ee71ac5177393a8b4bdafe278da87e75
Oleg Makarenko <olegomm@gmail.com>
2021-02-12 16:35:56 +0200

    update gitignore

--- scripts/build-clients.sh
+++ scripts/build-clients.sh
@@ -2,5 +2,5 @@
 echo "Running build of Node Rewards client..."
 rimraf src/models/NodeRewards/build src/models/NodeRewards/gen-src &&
 swagger-cli validate src/models/NodeRewards/openapi3.yml
-openapi-generator-cli generate -i src/models/NodeRewards/openapi3.yml -g typescript-fetch -o gen-src --additional-properties=typescriptThreePlus=true,withInterfaces=true,useSingleRequestParameter=false,supportsES6=true
+openapi-generator-cli generate -i src/models/NodeRewards/openapi3.yml -g typescript-fetch -o src/models/NodeRewards/gen-src --additional-properties=typescriptThreePlus=true,withInterfaces=true,useSingleRequestParameter=false,supportsES6=true
 exit 0
