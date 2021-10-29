3b0fc8060be73aadc993db1a93b20688d9fb823a
AnthonyLaw <yc-law1015@hotmail.com>
2021-10-29 19:29:18 +0800

    feat: add limit on node list search

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response } from 'express';
-import { DataBase } from '@src/services/DataBase';
+import { DataBase, NodeSearchCriteria } from '@src/services/DataBase';
 import { NotFoundError, InternalServerError, UnsupportedFilterError } from '@src/infrastructure/Error';
 import { symbol } from '@src/config';
 
@@ -7,51 +7,46 @@ enum nodeFilter {
 	Preferred = 'preferred',
 	Suggested = 'suggested',
 }
+
 export class Routes {
 	static register = async (app: Express) => {
 		app.get('/nodes', async (req: Request, res: Response) => {
-			const { filter } = req.query;
+			const { filter, limit } = req.query;
 
-			// if filter is empty, it will return full list
-			if (!filter) {
-				return DataBase.getNodeList()
-					.then((nodes) => res.send(nodes))
-					.catch((error) => InternalServerError.send(res, error));
-			}
+			let searchCriteria: NodeSearchCriteria = {
+				filter: {},
+				limit: Number(limit) || 0,
+			};
 
 			// Return error message filter is not support
-			if (!(filter === nodeFilter.Preferred || filter === nodeFilter.Suggested)) {
+			if (!(filter === nodeFilter.Preferred || filter === nodeFilter.Suggested || !filter)) {
 				return UnsupportedFilterError.send(res, filter as string);
 			}
 
 			// ?filter=preferred
-			// return list config by admin.
+			// it filter by host / domain name config by admin.
 			if (filter === nodeFilter.Preferred) {
-				const nodeFilter = {
+				Object.assign(searchCriteria.filter, {
 					host: { $in: symbol.PREFERRED_NODES.map((node) => new RegExp(`^.${node}`, 'i')) },
 					'apiStatus.isAvailable': true,
 					'apiStatus.nodeStatus.apiNode': 'up',
 					'apiStatus.nodeStatus.db': 'up',
-				};
-
-				return DataBase.getNodeList(nodeFilter)
-					.then((nodes) => res.send(nodes))
-					.catch((error) => InternalServerError.send(res, error));
+				});
 			}
 
 			// ?filter=suggested
 			// it filter health nodes
 			if (filter === nodeFilter.Suggested) {
-				const nodeFilter = {
+				Object.assign(searchCriteria.filter, {
 					'apiStatus.isAvailable': true,
 					'apiStatus.nodeStatus.apiNode': 'up',
 					'apiStatus.nodeStatus.db': 'up',
-				};
-
-				return DataBase.getNodeList(nodeFilter)
-					.then((nodes) => res.send(nodes))
-					.catch((error) => InternalServerError.send(res, error));
+				});
 			}
+
+			return DataBase.getNodeList(searchCriteria)
+				.then((nodes) => res.send(nodes))
+				.catch((error) => InternalServerError.send(res, error));
 		});
 
 		app.get('/nodesHostDetail', (req: Request, res: Response) => {
