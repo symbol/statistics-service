39d948bb723cf360f0e07ea7deab83b377c786f5
Baha <baha@symbol.dev>
2022-05-03 19:16:41 +0300

    [statistics-service] fix: PR feedbacks fixed, unit tests enhanced

--- test/ApiNodeService.test.ts
+++ test/ApiNodeService.test.ts
@@ -1,31 +1,88 @@
 import { expect } from 'chai';
-import { stub } from 'sinon';
+import { stub, restore } from 'sinon';
 import { ApiNodeService } from '@src/services/ApiNodeService';
 
 describe('ApiNodeService', () => {
-	it('getStatus proceeds even some of the api calls hangs', async () => {
+	const host = 'localhost';
+	const hostURL = `https://${host}:3001`;
+	const webSocketStatus = { isAvailable: true, wss: true, url: `wss://${host}:3001` };
+	const serverInfo = {
+		restVersion: '2.4.0',
+		sdkVersion: '2.4.1',
+		deployment: {
+			deploymentTool: 'symbol-bootstrap',
+			deploymentToolVersion: '1.1.6',
+			lastUpdatedDate: '2022-03-16',
+		},
+	};
+	const chainInfo = {
+		scoreHigh: '0',
+		scoreLow: '3553661314979960997',
+		height: '355232',
+		latestFinalizedBlock: {
+			finalizationEpoch: 495,
+			finalizationPoint: 16,
+			height: '355204',
+			hash: 'ACE7C54D9DEB625151157D2E0A0EC4CD92235A95A0997FF87FA8DE743A2E46B8',
+		},
+	};
+
+	afterEach(() => {
+		restore();
+	});
+
+	it('getStatus returns data successfully', async () => {
+		// Arrange:
+		const nodeInfo = {
+			version: 16777987,
+			publicKey: '3460D29534CA997D6A74BEBB93F38356833B806E1A35F700EBE08D57FC8D3FED',
+			networkGenerationHashSeed: '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836',
+			roles: 3,
+			port: 7900,
+			networkIdentifier: 152,
+			host: host,
+			friendlyName: host,
+			nodePublicKey: '7F95C44319D02FF33F852142D78D85679B1B82EB48194CCCFBB6749ED75653DC',
+		};
+
+		const nodeStatus = {
+			apiNode: 'up',
+			db: 'up',
+		};
+
+		const finalization = {
+			height: Number.parseInt(chainInfo.latestFinalizedBlock.height),
+			epoch: chainInfo.latestFinalizedBlock.finalizationEpoch,
+			point: chainInfo.latestFinalizedBlock.finalizationPoint,
+			hash: chainInfo.latestFinalizedBlock.hash,
+		};
+
+		stub(ApiNodeService, 'getNodeChainInfo').returns(Promise.resolve(chainInfo));
+		stub(ApiNodeService, 'webSocketStatus').returns(Promise.resolve(webSocketStatus));
+		stub(ApiNodeService, 'getNodeServer').returns(Promise.resolve(serverInfo));
+		stub(ApiNodeService, 'getNodeInfo').returns(Promise.resolve(nodeInfo));
+		stub(ApiNodeService, 'getNodeHealth').returns(Promise.resolve(nodeStatus));
+		stub(ApiNodeService, 'isHttpsEnabled').returns(Promise.resolve(true));
+
+		// Act:
+		const apiStatus = await ApiNodeService.getStatus(hostURL);
+
+		// Assert:
+		expect(apiStatus.webSocket).to.be.deep.equal(webSocketStatus);
+		expect(apiStatus.restGatewayUrl).to.be.equal(hostURL);
+		expect(apiStatus.isAvailable).to.be.equal(true);
+		expect(apiStatus.isHttpsEnabled).to.be.equal(true);
+		expect(apiStatus.nodeStatus).to.be.deep.equal(nodeStatus);
+		expect(apiStatus.chainHeight).to.equal(chainInfo.height);
+		expect(apiStatus.finalization).to.be.deep.equal(finalization);
+		expect(apiStatus.nodePublicKey).to.be.equal(nodeInfo.nodePublicKey);
+		expect(apiStatus.restVersion).to.be.equal(serverInfo.restVersion);
+		expect(apiStatus.lastStatusCheck).to.be.not.undefined;
+	});
+
+	it('getStatus proceeds even if some of the api calls hang', async () => {
 		// Arrange:
 		const webSocketStatus = { isAvailable: false, wss: false, url: undefined };
-		const serverInfo = {
-			restVersion: '2.4.0',
-			sdkVersion: '2.4.1',
-			deployment: {
-				deploymentTool: 'symbol-bootstrap',
-				deploymentToolVersion: '1.1.6',
-				lastUpdatedDate: '2022-03-16',
-			},
-		};
-		const chainInfo = {
-			height: '1173781',
-			scoreHigh: '6',
-			scoreLow: '18350901762684228126',
-			latestFinalizedBlock: {
-				finalizationEpoch: 817,
-				finalizationPoint: 8,
-				height: '1173756',
-				hash: '8B9CB1A0D275720AEA04B9CB373994F2CC3826256DECFC0DE999FE7ED42F3814',
-			},
-		};
 
 		stub(ApiNodeService, 'getNodeChainInfo').returns(Promise.resolve(chainInfo));
 		stub(ApiNodeService, 'webSocketStatus').returns(Promise.resolve(webSocketStatus));
@@ -44,11 +101,13 @@ describe('ApiNodeService', () => {
 		stub(ApiNodeService, 'getNodeServer').returns(Promise.resolve(serverInfo));
 
 		// Act:
-		const apiStatus = await ApiNodeService.getStatus('http://localhost:3000');
+		const apiStatus = await ApiNodeService.getStatus(hostURL);
 
 		// Assert:
-		expect(apiStatus.webSocket).to.be.eql(webSocketStatus);
-		expect(apiStatus.restVersion).to.be.eq('2.4.0');
-		expect(apiStatus.chainHeight).to.be.eq('1173781');
+		expect(apiStatus.nodePublicKey).to.be.undefined;
+		expect(apiStatus.nodeStatus).to.be.undefined;
+		expect(apiStatus.webSocket).to.be.deep.equal(webSocketStatus);
+		expect(apiStatus.restVersion).to.be.equal(serverInfo.restVersion);
+		expect(apiStatus.chainHeight).to.be.equal(chainInfo.height);
 	}).timeout(10_000);
 });
--- test/Http.test.ts
+++ test/Http.test.ts
@@ -1,25 +1,41 @@
 import { HTTP } from '@src/services/Http';
-import { expect } from 'chai';
-import { stub } from 'sinon';
+import { use, expect } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { stub, restore } from 'sinon';
 import axios from 'axios';
 
+use(chaiAsPromised);
+
 describe('Http', () => {
+	afterEach(() => {
+		restore();
+	});
+
 	it('Get method timeouts after given time', async () => {
 		// Arrange:
 		stub(axios, 'get').returns(
-			new Promise((_, reject) => {
+			new Promise((resolve) => {
 				setTimeout(() => {
-					reject(Error('timeout'));
+					resolve('success');
 				}, 5_000);
 			}),
 		);
 
-		// Act:
-		try {
-			await HTTP.get('SOME_FAKE_URL', { timeout: 2_000 });
-		} catch (err: any) {
-			// Assert:
-			expect(err.message).to.be.equal(HTTP.TIMEOUT_MSG);
-		}
+		// Act + Assert:
+		return expect(HTTP.get('SOME_FAKE_URL', { timeout: 2_000 })).to.eventually.be.rejectedWith(HTTP.TIMEOUT_MSG);
 	}).timeout(3000);
+
+	it('Get method returns data successfully', async () => {
+		// Arrange:
+		stub(axios, 'get').returns(
+			new Promise((resolve, _) => {
+				setTimeout(() => {
+					resolve('success');
+				}, 1_000);
+			}),
+		);
+
+		// Act + Assert:
+		return expect(HTTP.get('SOME_FAKE_URL', { timeout: 2_000 })).to.eventually.be.equal('success');
+	});
 });
