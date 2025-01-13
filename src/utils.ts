e436c0c47484d8f929ab24a2b77c4526836853f3
AnthonyLaw <anthony@symbol.dev>
2025-01-14 02:24:29 +0800

    [statistics-service] fix: removed unused code

--- src/utils.ts
+++ src/utils.ts
@@ -103,16 +103,6 @@ export const runTaskInChunks = async <T>(
 	}
 };
 
-export const splitByPredicate = <T>(predicate: (item: T) => boolean, arr: T[]): { filtered: T[]; unfiltered: T[] } => {
-	return arr.reduce(
-		(res, item: T) => {
-			res[predicate(item) ? 'filtered' : 'unfiltered'].push(item);
-			return res;
-		},
-		{ filtered: [] as T[], unfiltered: [] as T[] },
-	);
-};
-
 class TimeoutTimer {
 	private timeoutId?: NodeJS.Timeout;
 
